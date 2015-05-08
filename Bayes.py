96767c3764da18ba101ad9f0ba4539a8532705df
mz1991 <zuccon.matteo@gmail.com>
2015-05-08 13:54:43 +0200

    Added Falsi Positivi e Negativi

--- Bayes.py
+++ Bayes.py
@@ -2,14 +2,14 @@
 import csv
 import random
 import math
- 
+
 def loadCsv(filename):
 	lines = csv.reader(open(filename, "r"))
 	dataset = list(lines)
 	for i in range(len(dataset)):
 		dataset[i] = [float(x) for x in dataset[i]]
 	return dataset
- 
+
 def splitDataset(dataset, splitRatio):
 	trainSize = int(len(dataset) * splitRatio)
 	trainSet = []
@@ -23,35 +23,35 @@ def separateByClass(dataset):
 	separated = {}
 	for i in range(len(dataset)):
 		vector = dataset[i]
-		if (vector[-1] not in separated): 
+		if (vector[-1] not in separated):
 			separated[vector[-1]] = [] # -1 l'ultimo attributo è quello su cui voglio fare previsione
 		separated[vector[-1]].append(vector)
 	return separated
- 
+
 def mean(numbers):
 	return sum(numbers)/float(len(numbers))
- 
+
 def stdev(numbers):
 	avg = mean(numbers)
 	variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
 	return math.sqrt(variance)
- 
+
 def summarize(dataset):
 	summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
 	del summaries[-1]
 	return summaries
- 
+
 def summarizeByClass(dataset):
 	separated = separateByClass(dataset)
 	summaries = {}
 	for classValue, instances in separated.items():
 		summaries[classValue] = summarize(instances)
 	return summaries
- 
+
 def calculateProbability(x, mean, stdev):
 	exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
 	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
- 
+
 def calculateClassProbabilities(summaries, inputVector):
 	probabilities = {}
 	for classValue, classSummaries in summaries.items():
@@ -61,7 +61,7 @@ def calculateClassProbabilities(summaries, inputVector):
 			x = inputVector[i]
 			probabilities[classValue] *= calculateProbability(x, mean, stdev)
 	return probabilities
-			
+
 def predict(summaries, inputVector):
 	probabilities = calculateClassProbabilities(summaries, inputVector)
 	bestLabel, bestProb = None, -1
@@ -70,20 +70,27 @@ def predict(summaries, inputVector):
 			bestProb = probability
 			bestLabel = classValue
 	return bestLabel
- 
+
 def getPredictions(summaries, testSet):
 	predictions = []
 	for i in range(len(testSet)):
 		result = predict(summaries, testSet[i])
 		predictions.append(result)
 	return predictions
- 
+
 def getAccuracy(testSet, predictions):
-	correct = 0
+	correct = 0 # TODO: falsi positivi (ipotesi valida ma rifiutata) and falso negativo (ipotesi sbagliata ma accettata)
+	falsiPositivi = 0
+	falsiNegativi = 0
 	for i in range(len(testSet)):
 		if testSet[i][-1] == predictions[i]:
 			correct += 1
-	return (correct/float(len(testSet))) * 100.0
+		else:
+			if testSet[i][-1]==1 and predictions[i]==0:
+				falsiPositivi+=1
+			elif testSet[i][-1]==0 and predictions[i]==1:
+				falsiNegativi+=1
+	return (correct/float(len(testSet))) * 100.0 , falsiPositivi, falsiNegativi
 
 def main():
 	filename = 'data.csv'
@@ -95,7 +102,9 @@ def main():
 	summaries = summarizeByClass(trainingSet)
 	# test model
 	predictions = getPredictions(summaries, testSet)
-	accuracy = getAccuracy(testSet, predictions)
+	accuracy,falsiPositivi,falsiNegativi = getAccuracy(testSet, predictions)
 	print('Accuracy: {0}%'.format(accuracy))
+	print('Falsi Positivi: {0}'.format(falsiPositivi))
+	print('Falsi Negativi: {0}'.format(falsiNegativi))
 
 main()
